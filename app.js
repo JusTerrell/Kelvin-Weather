596e3dc1f5da3412cbbae2d657e3192c72f825cc
JusTerrell <54183992+JusTerrell@users.noreply.github.com>
2019-12-27 13:08:45 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// today's forecast
+const kelvin = 269;
+const celsius = kelvin - 273;
+// fahrenheit equation
+let fahrenheit = celsius * (9 / 5) + 32;
+// rounded fahrenheit temp
+fahrenheit = Math.floor(fahrenheit);
+// string interpolation below
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+let newton = celsius * (33 / 100);
+Math.floor(newton)
+console.log(`The temperature is ${newton} degrees Celsius`)
